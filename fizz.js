5cb0507ef9880048c79e9df46d49424ee000520a
Rubens Gomez Colmenares <rubens.gomez.c@munimacul.cl>
2022-07-08 16:12:30 -0400

    Cambio 1

---
+++ fizz.js
@@ -0,0 +1,22 @@
+//alert("Bienvenido");
+var numeros= 100;
+var divisible = false;
+for (var i= 1; i <=100; i++)
+{
+    divisible = false;
+    if(i % 3 == 0)
+{
+document.write("fizz");
+divisible= true;
+}
+ if (i % 5 ==0)
+{
+document.write("Buzz");
+divisible = true;
+}    
+else 
+{
+document.write(i);
+}
+document.write("<br/>");
+}
